bf9e636b7d80817633b1b865043a4797dd541ad6
younes <youn.henni@gmail.com>
2022-12-13 15:54:20 +0100

    Adds everything

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require("express");
+var bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+let data;
+
+app.post("/", (req, res) => {
+  data = req.body;
+  res.status(201).send("Hello World 3");
 });
 
+app.get("/", (req, res) => {
+  res.status(200).json(data);
+});
+
+app.use(express.static("frontend"));
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
